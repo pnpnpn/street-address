b9a36d8970ee4bd32ce817a144cfae8c836cc848
Patrick Hayes <phayes@votem.com>
2018-03-06 14:39:26 -0800

    Adding all recognized USA street suffix abbreviations

--- streetaddress/streetaddress.py
+++ streetaddress/streetaddress.py
@@ -12,9 +12,12 @@
 import re
 import six
 
+from . import abbrevs
+
 ########################################################################
 # StreetAddressParser
 ########################################################################
+
 
 class StreetAddressParser():
     def __init__(self):
@@ -27,21 +30,21 @@ class StreetAddressParser():
             'apt', 'apartment', 'apartme', 'apartemen', 'apartado', 'apartament',
             'room', 'rm', '#',
             'unit', 'un', 'unt'
-            ])
-        self.rec_st_nd_rd_th = re.compile(r'^\d+(st|nd|rd|th)$', flags=re.I|re.U)
-        self.rec_house_number = re.compile(r'^\d\S*$', flags=re.I|re.U)
+        ])
+        self.rec_st_nd_rd_th = re.compile(r'^\d+(st|nd|rd|th)$', flags=re.I | re.U)
+        self.rec_house_number = re.compile(r'^\d\S*$', flags=re.I | re.U)
 
     def parse(self, addr_str, skip_house=False):
         addr_str = addr_str.strip()
         res = {
-                'house' : None,
-                'street_name' : None,
-                'street_type' : None,
-                'street_full' : None,
-                'suite_num' : None,
-                'suite_type' : None,
-                'other' : None,
-                }
+            'house': None,
+            'street_name': None,
+            'street_type': None,
+            'street_full': None,
+            'suite_num': None,
+            'suite_type': None,
+            'other': None,
+        }
 
         tokens = addr_str.split()
         start_idx = 0
@@ -56,28 +59,28 @@ class StreetAddressParser():
                 res['house'] = six.text_type(self.text2num_dict[tokens[0].lower()])
                 start_idx = 1
             elif self.rec_st_nd_rd_th.search(tokens[0]):
-                #first token is actually a street number (not house)
-                start_idx = 0 
+                # first token is actually a street number (not house)
+                start_idx = 0
             elif self.rec_house_number.search(tokens[0]):
-                res['house'] = tokens[0] 
+                res['house'] = tokens[0]
                 start_idx = 1
             else:
-                #no house number
+                # no house number
                 start_idx = 0
 
             if res['house'] and len(tokens) >= 2 and tokens[1] == '1/2':
-                res['house'] += ' ' + tokens[1] 
+                res['house'] += ' ' + tokens[1]
                 start_idx = 2
 
         street_accum = []
         other_accum = []
-        is_in_state = 'street' #can be 'street', 'suite', 'other'
+        is_in_state = 'street'  # can be 'street', 'suite', 'other'
 
         for i in range(start_idx, len(tokens)):
             word = tokens[i]
-            #word = re.sub(r'[\.\,]+$', '', word, flags=re.I|re.U) 
+            #word = re.sub(r'[\.\,]+$', '', word, flags=re.I|re.U)
             while len(word) > 0 and (word[-1] == '.' or word[-1] == ','):
-                #truncate the trailing dot (for abbrev)
+                # truncate the trailing dot (for abbrev)
                 word = word[:-1]
             word_lw = word.lower()
 
@@ -96,14 +99,14 @@ class StreetAddressParser():
             elif is_in_state == 'suite':
                 res['suite_num'] = word
                 is_in_state = 'other'
-            elif is_in_state == 'other': 
+            elif is_in_state == 'other':
                 other_accum.append(word)
             else:
                 raise Exception('this state should never be reached')
 
         # TODO PO Box handling
         #acronym = lambda s : Regex(r"\.?\s*".join(s)+r"\.?")
-        #poBoxRef = ((acronym("po") | acronym("apo") | acronym("afp")) + 
+        # poBoxRef = ((acronym("po") | acronym("apo") | acronym("afp")) +
         #            Optional(CaselessLiteral("BOX"))) + Word(alphanums)("boxnumber")
 
         if street_accum:
@@ -114,39 +117,19 @@ class StreetAddressParser():
         if res['street_name'] and res['street_type']:
             res['street_full'] = res['street_name'] + ' ' + res['street_type']
         elif res['street_name']:
-            res['street_full'] = res['street_name'] 
+            res['street_full'] = res['street_name']
         elif res['street_type']:
-            res['street_full'] = res['street_type'] 
+            res['street_full'] = res['street_type']
 
         return res
 
-def get_abbrev_suffix_dict():
-    return {
-            'avenue' : 'ave',
-            'street' : 'st',
-            'boulevard': 'blvd',
-            'parkway': 'pkwy',
-            'highway': 'hwy',
-            'drive': 'dr',
-            'place': 'pl',
-            'expressway': 'expy',
-            'heights': 'hts',
-            'junction' : 'jct',
-            'center': 'ctr',
-            'circle' : 'cir',
-            'cove' : 'cv',
-            'lane' : 'ln',
-            'road' : 'rd',
-            'court' : 'ct',
-            'square' : 'sq',
-            'loop' : 'lp',
-            'terrace': 'trc',
-            }
 
+def get_abbrev_suffix_dict():
+    return abbrevs.USA_ABBREVS
 
 
 def get_text2num_dict():
-    return  {
+    return {
         'zero': 0,
         'one': 1,
         'two': 2,
@@ -175,7 +158,7 @@ def get_text2num_dict():
         'seventy': 70,
         'eighty': 80,
         'ninety': 90,
-        } 
+    }
 
 
 ########################################################################
@@ -183,24 +166,25 @@ def get_text2num_dict():
 ########################################################################
 class StreetAddressFormatter():
     def __init__(self):
-        #abbreviate west, east, north, south?
+        # abbreviate west, east, north, south?
         self.abbrev_suffix_map = get_abbrev_suffix_dict()
-        self.street_type_set = set(self.abbrev_suffix_map.keys()) | set(self.abbrev_suffix_map.values())
+        self.street_type_set = set(self.abbrev_suffix_map.keys()) | set(
+            self.abbrev_suffix_map.values())
         self.abbrev_direction_map = {
-            'east' : 'E',
-            'west' : 'W', 
-            'north' : 'N',
-            'south' : 'S',
-            }
+            'east': 'E',
+            'west': 'W',
+            'north': 'N',
+            'south': 'S',
+        }
 
-        for k,v in self.abbrev_suffix_map.items():
+        for k, v in self.abbrev_suffix_map.items():
             self.abbrev_suffix_map[k] = v.title()
 
         TH_or_str = '|' . join(self.street_type_set)
-        self.re_TH= re.compile(r'\b(\d+)\s+(%s)\.?$' % TH_or_str, flags=re.I|re.U)
+        self.re_TH = re.compile(r'\b(\d+)\s+(%s)\.?$' % TH_or_str, flags=re.I | re.U)
 
     def st_nd_th_convert(self, num_str):
-        if len(num_str) >= 2 and (num_str[-2:] =='11' or num_str[-2:] =='12'):
+        if len(num_str) >= 2 and (num_str[-2:] == '11' or num_str[-2:] == '12'):
             return num_str + 'th'
         elif num_str[-1] == '1':
             return num_str + 'st'
@@ -212,7 +196,7 @@ class StreetAddressFormatter():
             return num_str + 'th'
 
     def append_TH_to_street(self, addr):
-        #street,avenue needs to be the last word
+        # street,avenue needs to be the last word
         addr = addr.strip()
         match = self.re_TH.search(addr)
         if match:
@@ -226,9 +210,9 @@ class StreetAddressFormatter():
             return addr
 
         for i in range(len(word_lst) - 1):
-            word = word_lst[i].lower() 
-            #should have a digit after direction, e.g. "West 23rd" 
-            if word in self.abbrev_direction_map and word_lst[i+1][0].isdigit():
+            word = word_lst[i].lower()
+            # should have a digit after direction, e.g. "West 23rd"
+            if word in self.abbrev_direction_map and word_lst[i + 1][0].isdigit():
                 word_lst[i] = self.abbrev_direction_map[word]
         addr = ' ' . join(word_lst)
         return addr
@@ -244,10 +228,8 @@ class StreetAddressFormatter():
             pos_lst = range(len(word_lst))
 
         for p in pos_lst:
-            word = re.sub(r'\.$', '', word_lst[p]).lower() #get rid of trailing period
+            word = re.sub(r'\.$', '', word_lst[p]).lower()  # get rid of trailing period
             if word in self.abbrev_suffix_map:
                 word_lst[p] = self.abbrev_suffix_map[word]
         addr = ' ' . join(word_lst)
         return addr
-
-
